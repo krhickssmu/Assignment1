3aaaaaa24bce37514af97b272cdebaea2c624817
roxanad1 <167349781+roxanad1@users.noreply.github.com>
2024-09-24 17:43:46 -0500

    Update QueueOperations.java
    deleted unnecessary commented code

--- QueueOperations.java
+++ QueueOperations.java
@@ -55,36 +55,4 @@ public class QueueOperations<Item> implements Iterable<Item> {
             return item;
         }
     }
-
-//    public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.println("\nIn one line, enter 1 digit integers and add P next to it (for push to top),\n minus sign for pop,\n and number with E to add to queue (for enqueue); ex. 1P pushes 1:\n");
-//        String input = scan.nextLine();
-//
-//        Steque<String> item = new Steque<String>();
-//
-//        for (int i = 0; i < input.length(); i++){
-//            String num = input.charAt(i) + "";
-//            if (input.charAt(i) != '-' && (input.charAt(i+1) == 'P' || input.charAt(i+1) == 'p')){
-//
-//                item.push(num);
-//                System.out.println(" push " + num);
-//            }
-//            else if (input.charAt(i) != '-' && (input.charAt(i+1) == 'E' || input.charAt(i+1) == 'e')){
-//                item.enqueue(num);
-//                System.out.println(" enqueue " + num);
-//            }
-//            if (input.charAt(i) == '-'){
-//
-//                try{
-//                    System.out.println(" pop " + item.pop()); //pops first input
-//                } catch (Exception e) {
-//                    System.out.println("Can't redirect to null element, can't pop anymore");
-//                }
-//
-//            }
-//        }
-//    }
-
 }
